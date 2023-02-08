2cdca53e02c7d9ae1c2c00744d7f0e037dcd218c
Joonhoo <mnc5@naver.com>
2023-02-08 22:58:23 +0900

    패키지 week1 변경

--- src/week1/p11659.java
+++ src/week1/p11659.java
@@ -1,3 +1,4 @@
+package week1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,16 +12,19 @@ public class p11659 {
 	    int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 	    // 구간 합 저장 배열
 	    int[] sum = new int[input[0] + 1];
-	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    for (int i = 0; i < input[0]; i++) {
-			sum[i + 1] = sum[i] + parseInt(st.nextToken());
+	    for (int i = 0; i < sum.length; i++) {
+			System.out.print(i + " ");
 		}
-	    for (int i = 0; i < input[1]; i++) {
-		    int[] index = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		    int value = sum[index[1]] - sum[index[0] - 1];
-		    answer.append(value + "\n");
-		}
-	    System.out.println(answer);
+//	    StringTokenizer st = new StringTokenizer(br.readLine());
+//	    for (int i = 0; i < input[0]; i++) {
+//			sum[i + 1] = sum[i] + parseInt(st.nextToken());
+//		}
+//	    for (int i = 0; i < input[1]; i++) {
+//		    int[] index = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//		    int value = sum[index[1]] - sum[index[0] - 1];
+//		    answer.append(value + "\n");
+//		}
+//	    System.out.println(answer);
 	}
 	
 	static int parseInt(String input) {
